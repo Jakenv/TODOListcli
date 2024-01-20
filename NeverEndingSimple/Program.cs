899fdd145ca43bd30896a1d85cc76821107cc214
byso <bartoszmarciniak7258@gmail.com>
2024-01-20 16:29:29 +0100

    Added removing, but it does not work as it should

--- NeverEndingSimple/Program.cs
+++ NeverEndingSimple/Program.cs
@@ -40,6 +40,9 @@ do
         menuSelection = input.ToLower();
     }
 
+    bool correctInput = false;
+    string anotherEntry;
+    var todoCount = 0;
     switch (menuSelection)
         {
             case "s":
@@ -52,8 +55,8 @@ do
                break;
 
            case "a":
-                string anotherEntry = "y";
-                int todoCount = 0;
+                anotherEntry = "y";
+                todoCount = 0;
                 for (int i = 0; i < maxTodos; i++)
                 {
                     if (listOfTodo[i] != $"{i}: ")
@@ -61,14 +64,10 @@ do
                         todoCount += 1;
                     }
                 }
-
-                if (todoCount < maxTodos)
-                    Console.WriteLine($"{todoCount}We can manage {(maxTodos - todoCount)} more.");
                 
                 while (anotherEntry == "y" && todoCount < maxTodos)
                 {
                     string todoWords = "";
-                    bool correctInput;
                     do
                     {
                         Console.WriteLine("What do you want to add?");
@@ -110,12 +109,69 @@ do
                 Console.ReadKey();
                 break;
             case "r":
+                anotherEntry = "y";
+                todoCount = 0;
+                correctInput = false; 
+                
                 for (int i = 0; i < maxTodos; i++)
                 {
-                    Console.WriteLine(listOfTodo[i]);
+                    if (listOfTodo[i] != $"{i}: ")
+                    {
+                        todoCount -= 1;
+                    }
                 }
                 
-                Console.WriteLine("dupa");
+                while (anotherEntry == "y" && todoCount < maxTodos)
+                {
+                    do
+                    {
+                        for (var i = 0; i < maxTodos; i++)
+                            Console.WriteLine(listOfTodo[i]);
+                        
+                        Console.WriteLine("Which TODO you want to remove?");
+                        input = Console.ReadLine();
+                        if (input != null && int.TryParse(input, out var index))
+                        {
+                            if (index < maxTodos)
+                            {
+                                listOfTodo[index].Remove(index);
+                                Console.WriteLine($"You removed {index} {listOfTodo[index]}");
+                                correctInput = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Number must be between 0 and 5");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect inut, must declare number");
+                            correctInput = false;
+                        }
+                    } while (correctInput == false);
+                    
+                    todoCount -= 1;
+                    
+                    if (todoCount < maxTodos)
+                    {
+                        Console.WriteLine("Do you want to remove another entry?");
+                        do
+                        {
+                            input = Console.ReadLine();
+                            if (input != null)
+                            {
+                                anotherEntry = input.ToLower();
+                            }
+                                            
+                        } while (anotherEntry != "y" && anotherEntry != "n");
+                    }
+                }
+                
+                if (todoCount <= maxTodos)
+                {
+                    Console.WriteLine("Entries are empty");
+                }
+
                 Console.WriteLine("Press enter to continue"); 
                 Console.ReadKey();
                 break;
--- NeverEndingSimple/obj/Debug/net8.0/NeverEndingSimple.AssemblyInfo.cs
+++ NeverEndingSimple/obj/Debug/net8.0/NeverEndingSimple.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeverEndingSimple")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01edf892d32eed9e247b54080be65a3ce6cd550")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeverEndingSimple")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeverEndingSimple")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
